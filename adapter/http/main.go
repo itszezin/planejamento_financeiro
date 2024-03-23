65a178166ff759b8c97251141e8be36eea4c6f55
itszezin <josecarlosneto166@gmail.com>
2024-03-23 02:09:52 -0300

    Add prometheus integration

--- adapter/http/main.go
+++ adapter/http/main.go
@@ -6,14 +6,16 @@ import (
 
 	"github.com/itszezin/planejamento_financeiro/adapter/http/actuator"
 	"github.com/itszezin/planejamento_financeiro/adapter/http/transaction"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func Init() {
-
 	http.HandleFunc("/transactions", transaction.GetTransactions)
 	http.HandleFunc("/transactions/create", transaction.CreateATransaction)
 	http.HandleFunc("/health", actuator.Health)
-	http.ListenAndServe(":8080", nil)
 
-	fmt.Printf("Servidor iniciado em http://localhost:8080")
+	http.Handle("/metrics", promhttp.Handler())
+
+	fmt.Printf("Servidor iniciado em http://localhost:8080\n")
+	http.ListenAndServe(":8080", nil)
 }
--- go.mod
+++ go.mod
@@ -2,7 +2,14 @@ module github.com/itszezin/planejamento_financeiro
 
 go 1.22.0
 
+require github.com/prometheus/client_golang v1.19.0
+
 require (
-	golang.org/x/lint v0.0.0-20210508222113-6edffad5e616 // indirect
-	golang.org/x/tools v0.19.0 // indirect
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/prometheus/client_model v0.6.0 // indirect
+	github.com/prometheus/common v0.51.0 // indirect
+	github.com/prometheus/procfs v0.13.0 // indirect
+	golang.org/x/sys v0.18.0 // indirect
+	google.golang.org/protobuf v1.33.0 // indirect
 )
